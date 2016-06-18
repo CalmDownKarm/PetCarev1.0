236059fb91a0835e86a15fa820e06a63a804ef20
Karmanya <karmanya.aggarwal.14cse@bml.edu.in>
2016-06-18 20:36:40 +0530

    db works

--- app/src/main/java/teamfirefighters/petcarev10/MainActivity.java
+++ app/src/main/java/teamfirefighters/petcarev10/MainActivity.java
@@ -2,6 +2,8 @@ package teamfirefighters.petcarev10;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         editor.putBoolean("DB_READY",true);
         editor.commit();*/
 
-       // DB_READY_FLAG=Flag.getBoolean("DB_READY",false);
+        DB_READY_FLAG=Flag.getBoolean("DB_READY",false);
         if(DB_READY_FLAG) {//Data has been parsed
             Log.d("DB READY", "True");
 
@@ -28,9 +30,28 @@ public class MainActivity extends AppCompatActivity {
             abc.execute();
             Log.d("DB_READY", "Called ASYNC THREAD");
         }
-        //if(DB_READY_FLAG){//Check Flag Again to ensure that the data
+        DB_READY_FLAG=Flag.getBoolean("DB_READY",false);
+        if(DB_READY_FLAG){//Check Flag Again to ensure that the data
+            CardDBHelper cdbhelper=new CardDBHelper(getApplicationContext());
+            SQLiteDatabase db = cdbhelper.getReadableDatabase();
+            String[] projection={
+                    CardDBContract.CardTable.COLUMN_NAME_CARD_ID,
+                    CardDBContract.CardTable.COLUMN_NAME_CLASSIFICATION,
+                    CardDBContract.CardTable.COLUMN_NAME_SUBCLASSIFICATION,
+                    CardDBContract.CardTable.COLUMN_NAME_SUBSUBCLASSIFICATION
+            };
+            String sortOrder = CardDBContract.CardTable.COLUMN_NAME_SUBSUBCLASSIFICATION + " DESC";
+            Cursor c = db.query(CardDBContract.CardTable.TABLE_NAME,projection,null,null,null,null,sortOrder);
+            if(c!=null){
+                c.moveToFirst();
+                int foo = c.getInt(c.getColumnIndexOrThrow(CardDBContract.CardTable.COLUMN_NAME_CARD_ID));
+                String nerdy = c.getString(c.getColumnIndexOrThrow(CardDBContract.CardTable.COLUMN_NAME_CLASSIFICATION));
+                Log.d("ALL THIS SHIT","CLASSIFICATION"+nerdy+"ID: "+foo);
+            }else
+                Log.d("ALL THIS SHIT","CURSOR IS NULL");
 
-        //}
+
+        }
 
 
     }
