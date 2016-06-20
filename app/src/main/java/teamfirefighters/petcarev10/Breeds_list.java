a909b69544e2064aadf4ce89f0c63313052649f1
Karmanya <karmanya.aggarwal.14cse@bml.edu.in>
2016-06-20 18:35:07 +0530

    breed cursor works

--- app/src/main/java/teamfirefighters/petcarev10/Breeds_list.java
+++ app/src/main/java/teamfirefighters/petcarev10/Breeds_list.java
@@ -1,8 +1,14 @@
 package teamfirefighters.petcarev10;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Breeds_list extends AppCompatActivity {
 
@@ -13,8 +19,33 @@ public class Breeds_list extends AppCompatActivity {
 
         String data = getIntent().getExtras().getString("Category");
         String Category_name = getIntent().getExtras().getString("Category");
-        final TextView foo = (TextView) findViewById(R.id.textView);
-
-        foo.setText(data);
+        getBreedsFromDb(Category_name);
     }
+    private List<String> getBreedsFromDb(String foo){
+        List<String> Breeds = new ArrayList<String>();
+        //Check Flag Again to ensure that the database is ready
+            Log.d("HELLO WORLD","IN GET BREEDS");
+            CardDBHelper cdbhelper=new CardDBHelper(getApplicationContext());
+            SQLiteDatabase db = cdbhelper.getReadableDatabase();
+            String[] projection={
+                    CardDBContract.CardTable.COLUMN_NAME_SUBCLASSIFICATION
+            };
+            String selection = CardDBContract.CardTable.COLUMN_NAME_CLASSIFICATION+"=?";
+            String[] selectionArgs = {foo};
+            String sortOrder = CardDBContract.CardTable.COLUMN_NAME_SUBCLASSIFICATION + " DESC";
+            Cursor c = db.query(true, CardDBContract.CardTable.TABLE_NAME,projection,selection,selectionArgs,null,null,sortOrder,null);
+            Log.d("TITS Cursor","SHIT IN BREEDS");
+            if(c!=null){
+                Log.d("TITS Cursor BREEDS",c.toString());
+                for(c.moveToFirst();!c.isAfterLast();c.moveToNext()){
+                    String temp = c.getString(c.getColumnIndexOrThrow(CardDBContract.CardTable.COLUMN_NAME_SUBCLASSIFICATION));
+                    Breeds.add(temp);
+                }
+        }
+        Log.d("ALL BREEDS",Breeds.toString());
+        return Breeds;
+    }
+
+
 }
+
