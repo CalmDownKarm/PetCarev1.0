6bd5e3b2a24a41ba9bfe48c6db24e5d093335244
Hitkul <hitkuljangid@gmail.com>
2016-06-29 22:56:00 +0530

    back stack fixed

--- app/src/main/java/teamfirefighters/petcarev10/Home_Activity.java
+++ app/src/main/java/teamfirefighters/petcarev10/Home_Activity.java
@@ -331,7 +331,6 @@ public class Home_Activity extends AppCompatActivity {
             toolbarPanelLayout.closePanel();;
             return;
         } else {
-            Log.d("BOOBS","yoyooyoyooyoy");
             finish();
         }
     }
